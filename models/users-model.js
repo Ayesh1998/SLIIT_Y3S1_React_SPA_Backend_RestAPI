28f3a47646b059c463a1ab78bd5dc00162d1d96f
Ayesh1998 <ayeshlak1998@gmail.com>
2020-03-18 14:49:27 +0530

    Added users model

---
+++ models/users-model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  teleNo: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Users", userSchema);
