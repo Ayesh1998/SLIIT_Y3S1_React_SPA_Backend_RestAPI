d1cf244bc8ec01d5f103e87f1e4c26f0e724b340
Ayesh1998 <ayeshlak1998@gmail.com>
2020-03-18 09:33:23 +0530

    Finished adding login and signup conrollers and routes

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const HttpError = require("./models/http-errors");
+
+const userRoutes = require("./routes/user-routes");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/users", userRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route.", 404);
+});
 
 app.listen(5000);
