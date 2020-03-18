0919349f2fb99abd5045ad7a65c860f282c06d94
Ayesh1998 <ayeshlak1998@gmail.com>
2020-03-18 06:27:29 +0530

    Added folder structure and app.js with server initialization

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(5000);
