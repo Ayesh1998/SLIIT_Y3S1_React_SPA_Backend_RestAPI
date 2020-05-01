4bb96c7c96f6b845b3e552714e59992257d220de
Tharinda <tharindarajapakshe@gmail.com>
2020-05-01 16:31:41 +0530

    adding routes and empty methods for delete, get, get_list methods for categories and store managers + minor syntax changes of the existing codes

--- controller/store-manager-controller.js
+++ controller/store-manager-controller.js
@@ -1,8 +1,8 @@
-const httpError = require("../models/http-errors");
-const User = require("../models/users-model");
+const httpError = require("../models/http-errors")
+const User = require("../models/users-model")
 
 const addStoreManager = async (req, res, next) => {
-  let existingUser;
+  let existingUser
 
   const {
     firstName,
@@ -12,26 +12,26 @@ const addStoreManager = async (req, res, next) => {
     password,
     passwordResetQuestion,
     answer
-  } = req.body;
+  } = req.body
 
   try {
     existingUser = await User.findOne({
       email: email
-    });
+    })
   } catch (err) {
-    const error = new httpError("Unexpected internal server error occurred, please try again later.", 500);
+    const error = new httpError("Unexpected internal server error occurred, please try again later.", 500)
     res.json({
       message: "Unexpected internal server error occurred, please try again later."
-    });
-    return next(error);
+    })
+    return next(error)
   }
 
   if (existingUser) {
-    const error = new httpError("A user with the same email already exists.", 409);
+    const error = new httpError("A user with the same email already exists.", 409)
     res.json({
       message: "A user with the same email already exists."
-    });
-    return next(error);
+    })
+    return next(error)
   }
 
   const newStoreManager = new User({
@@ -43,38 +43,38 @@ const addStoreManager = async (req, res, next) => {
     passwordResetQuestion,
     answer,
     type: 'Store Manager'
-  });
+  })
 
   try {
-    await newStoreManager.save();
-    console.log(newStoreManager);
+    await newStoreManager.save()
+    console.log(newStoreManager)
     res.json({
       message: "New store manager added!"
-    });
+    })
   } catch (err) {
-    const error = new httpError("Unexpected internal server error occurred, please try again later.", 500);
+    const error = new httpError("Unexpected internal server error occurred, please try again later.", 500)
     res.json({
       message: "Unexpected internal server error occurred, please try again later."
-    });
-    return next(error);
+    })
+    return next(error)
   }
 
   res.status(201).json({
     category: newStoreManager.toObject({
       getters: true
     })
-  });
+  })
 
   res.json({
     message: "New store manager added successfully!",
     category: newStoreManager.toObject({
       getters: true
     })
-  });
-};
+  })
+}
 
 const updateStoreManager = async (req, res, next) => {
-  let user;
+  let user
 
   const {
     id,
@@ -82,47 +82,66 @@ const updateStoreManager = async (req, res, next) => {
     lastName,
     teleNo,
     email
-  } = req.body;
+  } = req.body
 
   try {
-    user = await User.findById(id);
+    user = await User.findById(id)
   } catch (err) {
-    const error = new httpError("Unexpected internal server error occurred, please try again later.", 500);
+    const error = new httpError("Unexpected internal server error occurred, please try again later.", 500)
     res.json({
       message: "Unexpected internal server error occurred, please try again later."
-    });
-    return next(error);
+    })
+    return next(error)
   }
 
-  user.firstName = firstName;
-  user.lastName = lastName;
-  user.teleNo = teleNo;
-  user.email = email;
+  user.firstName = firstName
+  user.lastName = lastName
+  user.teleNo = teleNo
+  user.email = email
 
   try {
-    await user.save();
-    console.log(user);
+    await user.save()
+    console.log(user)
   } catch (err) {
-    const error = new httpError("Unexpected internal server error occurred, please try again later.", 500);
+    const error = new httpError("Unexpected internal server error occurred, please try again later.", 500)
     res.json({
       message: "Unexpected internal server error occurred, please try again later."
-    });
-    return next(error);
+    })
+    return next(error)
   }
 
   res.status(200).json({
     user: user.toObject({
       getters: true
     })
-  });
+  })
 
   res.json({
     message: "Stock manager updated successfully!",
     user: user.toObject({
       getters: true
     })
-  });
-};
+  })
+}
 
-exports.addStoreManager = addStoreManager;
-exports.updateStoreManager = updateStoreManager;
+const deleteStoreManager = async (req, res, next) => {
+
+}
+
+const getStoreManager = async (req, res, next) => {
+
+}
+
+const getStoreManagerList = async (req, res, next) => {
+
+}
+
+const sendEmail = {
+  //
+}
+
+exports.addStoreManager = addStoreManager
+exports.updateStoreManager = updateStoreManager
+exports.deleteStoreManager = deleteStoreManager
+exports.getStoreManager = getStoreManager
+exports.getStoreManagerList = getStoreManagerList
--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const categoryController = require("../controller/category-controller");
-const storeManagerController = require("../controller/store-manager-controller");
+const express = require("express")
+const router = express.Router()
+const categoryController = require("../controller/category-controller")
+const storeManagerController = require("../controller/store-manager-controller")
 
-router.post("/AddCategory", categoryController.addCategory);
-router.post("/UpdateCategory", categoryController.updateCategory);
+router.post("/AddCategory", categoryController.addCategory)
+router.post("/UpdateCategory", categoryController.updateCategory)
+router.post("/DeleteCategory", categoryController.deleteCategory)
+router.post("/Category/:id", categoryController.getCategory)
+router.post("/Category", categoryController.getCategoryList)
 
-router.post("/AddStoreManager", storeManagerController.addStoreManager);
-router.post("/UpdateStoreManager", storeManagerController.updateStoreManager);
+router.post("/AddStoreManager", storeManagerController.addStoreManager)
+router.post("/UpdateStoreManager", storeManagerController.updateStoreManager)
+router.post("/DeleteStoreManager", storeManagerController.deleteStoreManager)
+router.post("/StoreManager/:id", storeManagerController.getStoreManager)
+router.post("/StoreManager", storeManagerController.getStoreManagerList)
 
-module.exports = router;
+module.exports = router
