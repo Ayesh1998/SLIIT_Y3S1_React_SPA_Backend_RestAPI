c8c9f7734c90d4c81c91306c573890f39992669b
harshani2427 <harshaninimanthika2427@gmail.com>
2020-05-12 20:13:53 +0530

    Complete the payment user details backend part with all crud

---
+++ models/payCardDetails-model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const Schema = mongoose.Schema
+
+const payCardSchema = new Schema({
+ email: {
+        type: String,
+        required: true,
+        unique: true
+      },
+  cardType: {
+    type: String,
+    required: true
+  },
+  cardNumber: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true,
+  collection: 'PayCardDetails'
+})
+
+payCardSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('PayCardDetails', payCardSchema)
