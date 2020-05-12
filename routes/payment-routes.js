199d7b325edf5d9307acd25cf99a4f993c8f1c60
harshani2427 <harshaninimanthika2427@gmail.com>
2020-05-12 21:46:00 +0530

    Complete the payment card details backend part with all crud

--- routes/payment-routes.js
+++ routes/payment-routes.js
@@ -9,5 +9,11 @@ router.get('/pay-user', PaymentsController.getPayUsers);
 router.get('/pay-user/:id', PaymentsController.getPayUser);
 router.delete('/pay-user/:id', PaymentsController.deletePayUser);
 
+router.post('/pay-card', PaymentsController.addPayCard);
+router.post('/pay-card/update/:id', PaymentsController.updatePayCard);
+router.get('/pay-card', PaymentsController.getPayCards);
+router.get('/pay-card/:id', PaymentsController.getPayCard);
+router.delete('/pay-card/:id', PaymentsController.deletePayCard);
+
 
 module.exports = router;
