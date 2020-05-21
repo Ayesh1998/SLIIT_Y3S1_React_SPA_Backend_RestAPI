c029138b505d40911bfa6616a3921af6ca197e27
Adeesha97 <adeeshakawshal1997@gmail.com>
2020-05-21 17:29:50 +0530

    Implemented product controller with add product method

---
+++ controller/product-controller.js
@@ -0,0 +1,56 @@
+const uuid = require("uuid/v4");
+const HttpError = require("../models/http-errors");
+const Product = require("../models/product-model");
+
+const addProduct = async (req, res, next) => {
+  const { title } = req.body;
+
+  const ProductItme = new Product({
+    title,
+  });
+  let existingProduct;
+
+  try {
+    existingProduct = await Product.findOne({ title: title });
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    res.json({
+      message: "Signing up failed, please try again later.",
+      login: 0,
+    });
+    return next(error);
+  }
+
+  if (existingProduct) {
+    const error = new HttpError(
+      "User already exists, please login instead.",
+      422
+    );
+    res.json({
+      message: "User already exists, please login instead.",
+      login: 0,
+    });
+    return next(error);
+  }
+
+  try {
+    console.log(ProductItme);
+    await ProductItme.save();
+    // res.json({ message: "Added Succeefully", added: 1 });
+  } catch (err) {
+    const error = new HttpError("Adding failed, please try again.", 500);
+    res.json({ message: "Adding failed, please try again.", added: 0 });
+    return next(error);
+  }
+
+  res.status(201).json({
+    product: ProductItme.toObject({ getters: true }),
+    message: "Added Succeefully",
+    added: 1,
+  });
+};
+
+exports.addProduct = addProduct;
