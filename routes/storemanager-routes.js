3e0a766d909f8de4c03e764b50252da1f1ed71b3
Adeesha97 <adeeshakawshal1997@gmail.com>
2020-05-21 17:30:17 +0530

    Implemented storemanager routes

--- routes/storemanager-routes.js
+++ routes/storemanager-routes.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
 
+const ProductController = require("../controller/product-controller");
+
+router.post("/addProduct", ProductController.addProduct);
+
+module.exports = router;
