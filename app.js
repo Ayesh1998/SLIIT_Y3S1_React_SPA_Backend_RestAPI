e843383f4cb8ff688d34da3f74cf5819d63715d9
Ayesh1998 <ayeshlak1998@gmail.com>
2020-05-23 11:11:44 +0530

    Resolving merge Conflict

--- app.js
+++ app.js
@@ -1,53 +1,67 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const HttpError = require('./models/http-errors')
-const UserRoutes = require('./routes/user-routes')
-const AdminRoutes = require('./routes/admin-routes')
-const PaymentRoutes = require('./routes/payment-routes')
-const CommentRoutes = require('./routes/comment-routes')
-const StoremanagerRoutes = require('./routes/storemanager-routes')
+const HttpError = require("./models/http-errors");
+const UserRoutes = require("./routes/user-routes");
+const AdminRoutes = require("./routes/admin-routes");
+const PaymentRoutes = require("./routes/payment-routes");
+const CommentRoutes = require("./routes/comment-routes");
+const StoremanagerRoutes = require("./routes/storemanager-routes");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: true,
   })
-)
+);
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use('/users', UserRoutes)
-app.use('/admin', AdminRoutes)
-app.use('/payments', PaymentRoutes)
-app.use('/comments', CommentRoutes)
-app.use("/storemanager", StoremanagerRoutes)
+app.use("/users", UserRoutes);
+app.use("/admin", AdminRoutes);
+app.use("/payments", PaymentRoutes);
+app.use("/comments", CommentRoutes);
+app.use("/storemanager", StoremanagerRoutes);
 
 app.use(() => {
-  throw new HttpError('Could not find this route.', 404)
-})
+  throw new HttpError("Could not find this route.", 404);
+});
 
-const uri = process.env.ATLAS_URI
-const port = process.env.PORT
+const uri = process.env.ATLAS_URI;
+const port = process.env.PORT;
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
-}
+  useCreateIndex: true,
+};
+
+//--------yakooo one eka uncomment krla, comment krhan onathi eka----------------//
+
+// mongoose
+//   .connect(uri, options)
+//   .then(() => {
+//     app.listen(port)
+//     console.log(`Server is running on port: ${port}`)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
 mongoose
-  .connect(uri, options)
+  .connect(
+    "mongodb+srv://ayesh:ayesh@ayesh-mongo-cluster-jqsxb.mongodb.net/sliit-y3s1-reactapp?retryWrites=true&w=majority"
+  )
   .then(() => {
-    app.listen(port)
-    console.log(`Server is running on port: ${port}`)
+    app.listen(5000);
+    console.log(`Server is running on port: ${port}`);
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
