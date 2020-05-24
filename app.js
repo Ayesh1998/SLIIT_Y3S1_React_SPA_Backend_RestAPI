69d68bbf206caf8762e34b8958b73bdb45a06ce9
Tharinda <tharindarajapakshe@gmail.com>
2020-05-24 09:53:45 +0530

    change gitignore, app.js and .env file

--- app.js
+++ app.js
@@ -1,67 +1,53 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-const HttpError = require("./models/http-errors");
-const UserRoutes = require("./routes/user-routes");
-const AdminRoutes = require("./routes/admin-routes");
-const PaymentRoutes = require("./routes/payment-routes");
-const CommentRoutes = require("./routes/comment-routes");
-const StoremanagerRoutes = require("./routes/storemanager-routes");
+const HttpError = require('./models/http-errors')
+const UserRoutes = require('./routes/user-routes')
+const AdminRoutes = require('./routes/admin-routes')
+const PaymentRoutes = require('./routes/payment-routes')
+const CommentRoutes = require('./routes/comment-routes')
+const StoremanagerRoutes = require('./routes/storemanager-routes')
 
-require("dotenv").config();
+require('dotenv').config()
 
-const app = express();
+const app = express()
 
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-);
+)
 
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json())
+app.use(cors())
 
-app.use("/users", UserRoutes);
-app.use("/admin", AdminRoutes);
-app.use("/payments", PaymentRoutes);
-app.use("/comments", CommentRoutes);
-app.use("/storemanager", StoremanagerRoutes);
+app.use('/users', UserRoutes)
+app.use('/admin', AdminRoutes)
+app.use('/payments', PaymentRoutes)
+app.use('/comments', CommentRoutes)
+app.use('/storemanager', StoremanagerRoutes)
 
 app.use(() => {
-  throw new HttpError("Could not find this route.", 404);
-});
+  throw new HttpError('Could not find this route.', 404)
+})
 
-const uri = process.env.ATLAS_URI;
-const port = process.env.PORT;
+const uri = process.env.ATLAS_URI
+const port = process.env.PORT || 5000
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-};
-
-// mongoose
-//   .connect(uri, options)
-//   .then(() => {
-//     app.listen(port)
-//     console.log(`Server is running on port: ${port}`)
-//   })
-//   .catch((error) => {
-//     console.log(error)
-//   })
-
-const PORT = process.env.PORT || 5000;
+}
 
 mongoose
-  .connect(
-    "mongodb+srv://ayesh:ayesh@ayesh-mongo-cluster-jqsxb.mongodb.net/sliit-y3s1-reactapp?retryWrites=true&w=majority"
-  )
+  .connect(uri, options)
   .then(() => {
-    app.listen(PORT);
-    console.log(`Server is running on port: ${PORT}`);
+    app.listen(port)
+    console.log(`Server is running on port: ${port}`)
   })
   .catch((error) => {
-    console.log(error);
-  });
+    console.log(error)
+  })
